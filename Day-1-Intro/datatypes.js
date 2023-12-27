56bd245de3d210fd18dacd35b544a63cdc2396bb
Eric-Obeng <obengeric.3958@gmail.com>
2023-12-27 23:30:11 +0000

    Day one exercises done

---
+++ Day-1-Intro/datatypes.js
@@ -0,0 +1,13 @@
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let fullName = "Eric Obeng";
+let human = true;
+let age;
+let sex = null;
+const years = NaN;
+
+console.log(typeof human);
+console.log(typeof fullName);
+console.log(typeof sex);
+console.log(typeof years);
+console.log(typeof age);
---
+++ Day-1-Intro/variable.js
@@ -0,0 +1,8 @@
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let name = "Eric";
+let human = true;
+let age;
+let sex = null;
+
+console.log(sex)
